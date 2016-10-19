4e590d8dbcf12be708768a1adbcbc9a8b5ac373b
Ilya91 <webmaster.1991@mail.ru>
2016-10-19 14:44:42 +0300

    new properties and methods

--- Classes/Address.php
+++ Classes/Address.php
@@ -2,8 +2,37 @@
 
 namespace Project\Classes;
 
-
+/**
+ * Class Address
+ * @package Project\Classes
+ */
 class Address
 {
+    public $streetAddress1;
+    public $streetAddress2;
 
+    public $cityName;
+
+    public $regionName;
+
+    public $index;
+
+    public $countryName;
+
+    /**
+     * Display an address in html
+     * @return string
+     */
+    function display(){
+        $output = "";
+        $output .= $this->streetAddress1;
+        if ($this->streetAddress2){
+            $output .= '<br>' . $this->streetAddress2;
+        }
+        $output .= '<br>';
+        $output .=  $this->cityName . ', ' . $this->regionName;
+        $output .=  ' ' . $this->index;
+        $output .=  '<br>' . $this->countryName;
+        return $output;
+    }
 }
